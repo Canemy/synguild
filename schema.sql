3ed4b0ffd7ec6e158587a39141d365e80f4b7e2d
Tauri-Sten Üksti <sten2409@gmail.com>
2018-08-19 01:33:12 +0300

    Fixed ranks

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
-drop table if exists applications;
+--drop table if exists applications;
 --drop table if exists users;
 --drop table if exists progression;
 --drop table if exists recruitment;
---drop table if exists roster;
+drop table if exists roster;
 
 
-create table applications (
-  id serial primary key ,
-  name text not null,
-  armory text not null,
-  logs text not null,
-  alts text not null,
-  attendance text not null,
-  farm text not null,
-  standby text not null,
-  personality text not null,
-  whyjoin text not null,
-  whypick text not null,
-  datetime date default current_timestamp
-);
+--create table applications (
+--  id serial primary key ,
+--  name text not null,
+--  armory text not null,
+--  logs text not null,
+--  alts text not null,
+--  attendance text not null,
+--  farm text not null,
+--  standby text not null,
+--  personality text not null,
+--  whyjoin text not null,
+--  whypick text not null,
+--  datetime date default current_timestamp
+--);
 --create table users (
 --  id serial primary key,
 --  name text not null,
@@ -45,15 +45,15 @@ create table applications (
 --  spec4 text,
 --  spec4_prio text
 --);
---create table roster (
---  name text primary key,
---  rank smallint not null,
---  class smallint not null,
---  level smallint not null,
---  thumbnail text not null,
---  description text,
---  show text not null
---);
+create table roster (
+  name text primary key,
+  rank smallint not null,
+  class smallint not null,
+  level smallint not null,
+  thumbnail text not null,
+  description text,
+  show text not null
+);
 --
 --
 --insert into users (name, pw_hash) values ('admin', 'pbkdf2:sha256:50000$59ys5BK5$37f9e6ecb115e927114a0a046b1717128de685451245d5e37dca1d277f009eab');
